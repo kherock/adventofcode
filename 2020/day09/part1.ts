2f487ca90ceca4d1b4e3cf8ff0c9eb0dd8451fac
Kyle Herock <kherock@gmail.com>
2020-12-09 12:24:24 -0500

    add day 9 solution

---
+++ 2020/day09/part1.ts
@@ -0,0 +1,33 @@
+import { readLines } from "https://deno.land/std/io/bufio.ts";
+
+import { binomialSum } from "../day01/part1.ts";
+
+export async function initializeWindow(
+  size: number,
+  input: AsyncIterableIterator<string>,
+): Promise<number[]> {
+  const window: number[] = [];
+  for (const _ of Array(size)) {
+    const { value } = await input.next();
+    window.push(Number(value));
+  }
+  return window;
+}
+
+if (import.meta.main) {
+  const input = readLines(await Deno.open("input.txt"));
+  const window = await initializeWindow(25, input);
+
+  for await (const line of input) {
+    const number = Number(line);
+
+    if (!binomialSum(number, window)) {
+      console.log(number);
+      Deno.exit(0);
+    }
+    window.shift();
+    window.push(number);
+  }
+
+  Deno.exit(1);
+}
---
+++ 2020/day09/part2.ts
@@ -0,0 +1,31 @@
+import { readLines } from "https://deno.land/std/io/bufio.ts";
+
+import { initializeWindow } from "./part1.ts";
+
+if (import.meta.main) {
+  const target = 675280050;
+
+  const input = readLines(await Deno.open("input.txt"));
+  const window = await initializeWindow(2, input);
+
+  while (window[window.length - 1] < target) {
+    const sum = window.reduce((acc, value) => acc + value);
+    if (sum === target) {
+      console.info(window);
+      window.sort((a, b) => a - b);
+      console.log(
+        `${window[0]} + ${window[window.length - 1]} = ${window[0] +
+          window[window.length - 1]}`,
+      );
+      Deno.exit(0);
+    }
+    if (sum < target) {
+      const { value } = await input.next();
+      window.push(Number(value));
+    } else if (sum > target) {
+      window.shift();
+    }
+  }
+
+  Deno.exit(1);
+}

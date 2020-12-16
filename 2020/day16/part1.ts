ab623d5804e9e44954af9bfb6a292f20a92e57c2
Kyle Herock <kherock@gmail.com>
2020-12-16 17:17:10 -0500

    implement day 16

---
+++ 2020/day16/part1.ts
@@ -0,0 +1,62 @@
+import { readLines } from "https://deno.land/std@0.80.0/io/bufio.ts";
+
+export interface Rule {
+  name: string;
+  ranges: [start: number, end: number][];
+}
+
+export enum ParseMode {
+  RULES = 0,
+  MY_TICKET = 1,
+  NEARBY_TICKETS = 2,
+}
+
+const rulePattern = /^([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$/;
+
+export function parseRule(input: string): Rule {
+  const [, name, ...ranges] = input.match(rulePattern)!;
+  const [start0, end0, start1, end1] = ranges.map(Number);
+  return {
+    name,
+    ranges: [[start0, end0], [start1, end1]],
+  };
+}
+
+export function parseTicket(input: string): number[] {
+  return input.split(",").map(Number);
+}
+
+export function inRanges(ranges: Rule["ranges"], value: number): boolean {
+  return ranges.some(([start, end]) => start <= value && value <= end);
+}
+
+if (import.meta.main) {
+  const notes = readLines(await Deno.open("input.txt"));
+  const rules: Rule[] = [];
+
+  let mode = ParseMode.RULES;
+  let errorRate = 0;
+  for await (const line of notes) {
+    if (line === "") {
+      mode++;
+      await notes.next();
+      continue;
+    }
+    switch (mode) {
+      case ParseMode.RULES:
+        rules.push(parseRule(line));
+        break;
+      case ParseMode.MY_TICKET:
+        break;
+      case ParseMode.NEARBY_TICKETS: {
+        for (const value of parseTicket(line)) {
+          if (!rules.some(({ ranges }) => inRanges(ranges, value))) {
+            errorRate += value;
+          }
+        }
+        break;
+      }
+    }
+  }
+  console.log(errorRate);
+}
---
+++ 2020/day16/part2.ts
@@ -0,0 +1,70 @@
+import { readLines } from "https://deno.land/std@0.80.0/io/bufio.ts";
+
+import { inRanges, ParseMode, parseRule, parseTicket, Rule } from "./part1.ts";
+
+export function* markInvalid(
+  rules: Rule[],
+  values: number[],
+): Generator<{ name: string; index: number }> {
+  const invalidRules = values.map((value) =>
+    rules.filter((rule) => !inRanges(rule.ranges, value))
+  );
+  if (invalidRules.every((arr) => arr.length < rules.length)) {
+    for (let index = 0; index < invalidRules.length; index++) {
+      for (const { name } of invalidRules[index]) {
+        yield { name, index };
+      }
+    }
+  }
+}
+
+if (import.meta.main) {
+  const notes = readLines(await Deno.open("input.txt"));
+  const rules: Rule[] = [];
+  const decoder = new Map<string, Set<number>>();
+  const myTicket: number[] = [];
+
+  let mode = ParseMode.RULES;
+  for await (const line of notes) {
+    if (line === "") {
+      if (mode++ === ParseMode.RULES) {
+        for (const rule of rules) {
+          decoder.set(rule.name, new Set(Array(rules.length).keys()));
+        }
+      }
+      await notes.next();
+      continue;
+    }
+    switch (mode) {
+      case ParseMode.RULES:
+        rules.push(parseRule(line));
+        break;
+      case ParseMode.MY_TICKET:
+        myTicket.push(...parseTicket(line));
+        break;
+      case ParseMode.NEARBY_TICKETS: {
+        for (const { name, index } of markInvalid(rules, parseTicket(line))) {
+          decoder.get(name)!.delete(index);
+        }
+        break;
+      }
+    }
+  }
+
+  const entries = [...decoder];
+  do {
+    entries.sort((a, b) => a[1].size - b[1].size);
+    const [name, candidates] = entries.shift()!;
+    if (candidates.size !== 1) {
+      throw new Error(`Could not deduce a single candidate for ${name}`);
+    }
+    const [knownIndex] = candidates;
+    for (const [, otherCandidates] of entries) {
+      otherCandidates.delete(knownIndex);
+    }
+  } while (entries.length);
+  const departureProduct = [...decoder].reduce((acc, [name, [index]]) => {
+    return name.startsWith("departure") ? acc * myTicket[index] : acc;
+  }, 1);
+  console.log(departureProduct);
+}
